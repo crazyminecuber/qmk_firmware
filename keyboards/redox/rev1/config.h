c3ec7eeadda495af2183736df2c66993db3353fa
Oskar <oskar@pop-os.localdomain>
2021-03-28 18:10:41 +0200

    Some tweeking

--- keyboards/redox/rev1/config.h
+++ keyboards/redox/rev1/config.h
@@ -96,5 +96,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 
-#define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TAP_TOGGLE 50  /* Tapping this number of times holds the key until tapped once again. */
 #define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
